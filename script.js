ae584c4fe3137b9facf7dc3f3030c133910482b6
Nikolai <nvanbaak@gmail.com>
2020-09-21 13:44:08 -0700

    commiting Jack's excellent HTML for him

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function callArticles() {
+
+    // get input from user fields
+
+        // keywords
+        
+        // year start
+
+        // year end
+
+    // Assemble ajax string
+
+    // Call ajax {
+
+    // } .then {
+
+        // for loop i < number of articles
+
+            // Run display article function
+    
+    // }
+
+}
