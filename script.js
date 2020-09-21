9c84bf3d18fe3392464b58b5c9667503650b1352
Nikolai <nvanbaak@gmail.com>
2020-09-21 13:38:24 -0700

    pseudocoded api call function

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function callArticles() {
+
+    // get input from user fields
+
+        // keywords
+        
+        // year start
+
+        // year end
+
+    // Assemble ajax string
+
+    // Call ajax {
+
+    // } .then {
+
+        // for loop i < number of articles
+
+            // Run display article function
+    
+    // }
+
+}
+
